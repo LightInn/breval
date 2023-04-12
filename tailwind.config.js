495e18775d9f2b1330a4bf4a1934a4c026042c80
breval <breval2000@live.fr>
2023-04-12 14:17:37 +0200

    🔧 corriger(renovate.json) : supprimer les espaces inutiles dans le fichier renovate.json ✨ fonctionnalité(globals.css) : améliorer la lisibilité et la cohérence du code CSS La suppression des espaces inutiles dans le fichier renovate.json permet d'améliorer la lisibilité du code et de le rendre plus facile à maintenir. Dans le fichier globals.css, les modifications apportées améliorent la lisibilité et la cohérence du code CSS en réorganisant les sélecteurs et en ajoutant des commentaires pour décrire les différentes parties du code.
    🎨 style(tailwind.config.js) : réorganiser les propriétés de l'objet colors
    ✨ fonctionnalité(tailwind.config.js) : ajouter des couleurs pour l'effet de lueur (glow)
    🎨 style(tailwind.config.js) : réorganiser les propriétés de l'objet spacing
    🎨 style(tailwind.config.js) : réorganiser les propriétés de l'objet borderWidth
    🎨 style(tailwind.config.js) : réorganiser les propriétés de l'objet skew
    Les propriétés de l'objet colors ont été réorganisées pour améliorer la lisibilité. De plus, deux nouvelles couleurs ont été ajoutées pour l'effet de lueur. Les propriétés de l'objet spacing, borderWidth et skew ont également été réorganisées pour améliorer la lisibilit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,43 +5,41 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      'body': ['"Be Vietnam Pro"', 'sans-serif'],
-      'display': ['"Noto Serif Display"', 'serif'],
-      'varela-round': ['"Varela Round"', 'sans-serif']
+      body: ['"Be Vietnam Pro"', 'sans-serif'],
+      display: ['"Noto Serif Display"', 'serif'],
+      'varela-round': ['"Varela Round"', 'sans-serif'],
     },
     extend: {
       colors: {
-        'slate': {
-          1000: "#000101",
-          900: "#151715",
+        slate: {
+          1000: '#000101',
+          900: '#151715',
         },
-        'glow' : {
-            500: "#93ffbb",
-            600: "#66ff9e",
-        }
+        glow: {
+          500: '#93ffbb',
+          600: '#66ff9e',
+        },
       },
       brightness: {
         10: '.10',
         25: '.25',
       },
       spacing: {
-        '112': '28rem',
-        '128': '32rem',
+        112: '28rem',
+        128: '32rem',
       },
       borderWidth: {
-        '20': '20px',
-        '40': '40px'
+        20: '20px',
+        40: '40px',
       },
       skew: {
-        '17': '17deg',
-        '20': '20deg',
-        '24': '24deg',
-        '28': '28deg',
-        '30': '30deg',
-        '32': '32deg',
+        17: '17deg',
+        20: '20deg',
+        24: '24deg',
+        28: '28deg',
+        30: '30deg',
+        32: '32deg',
       },
-
-
     },
   },
 
