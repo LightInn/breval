245b3cd1240fd6fcb6844bd32395c8fdb6c5ced0
Cinquin Andy <andy.cinquin@infotel.com>
2023-06-04 12:32:32 +0200

    landing page, section who am i

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,9 +28,7 @@ export default function Home() {
 					rel="stylesheet"
 				/>
 			</Head>
-
 			<Navbar />
-
 			<div className="relative z-20 flex h-screen w-screen items-center justify-center">
 				<Title />
 
@@ -80,10 +78,9 @@ export default function Home() {
 					</h2>
 				</div>
 			</div>
-
 			<section
 				className="relative z-20 flex
-      h-[30px] translate-y-[-30px] transform
+      h-[30px] translate-y-[-25px] transform
       snap-start items-center justify-center rounded-t-full bg-slate-900 drop-shadow-[0px_-10px_1px_-8px_rgba(0,0,0,1)]"
 			>
 				{/* @ts-ignore */}
@@ -91,33 +88,85 @@ export default function Home() {
 
 				{/*// Figure here*/}
 			</section>
-
 			<section
-				className="z-20 box-border flex h-[100vh] w-full snap-start
-      flex-col flex-wrap items-center justify-center bg-slate-900
-      text-sky-50  "
+				className="relative z-30 box-border flex h-[100vh] w-full
+			-translate-y-[30px] transform flex-col flex-wrap items-center
+			justify-center bg-slate-900 text-sky-50"
 			>
-				<h2 className="animate__animated animate__fadeInDown m-8 text-xl font-medium xl:text-3xl">
-					Bonjour, je m'appelle
-					<span className="text-glow-500">&nbsp; Bréval Le Floch</span>
-				</h2>
-				<p
-					className="text-2xs animate__animated animate__fadeInUp m-8 lg:max-w-screen-lg
-                 xl:m-[150px] xl:text-xl"
-				>
-					Je suis un étudiant en alternance qui habite à Nantes. Dès mon plus
-					jeune âge, j'ai été fasciné par les possibilités infinies des
-					ordinateurs et des nouvelles technologies. Alors que je continue
-					d'apprendre et d'explorer cet univers en constant évolution, je suis
-					motivé par ma passion de découvrir de nouveaux mondes et leurs
-					nouvelles règles et mes rêves."
-				</p>
+				<div className={'mx-auto h-full max-w-7xl'}>
+					<div
+						className={
+							'w-full lg:flex lg:h-full lg:items-center lg:justify-center'
+						}
+					>
+						<div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+							<div className="lg:pl-20">
+								<div className="flex max-w-xs px-2.5 lg:max-w-none">
+									<div
+										className={
+											'w-auto rotate-2 rounded-2xl bg-slate-800/30 p-10 shadow-2xl'
+										}
+									>
+										<Image
+											src={'/breval.png'}
+											alt="Breval Le Floch Avatar"
+											width={250}
+											height={250}
+											className="aspect-square object-cover object-center"
+										/>
+									</div>
+								</div>
+							</div>
+							<div className="flex h-full flex-col items-start justify-center lg:order-first lg:row-span-2">
+								<h1 className="text-4xl font-bold tracking-tight lg:text-5xl">
+									{`Bonjour, je m'appelle `}
+									<span className="text-glow-500">
+										Bréval&nbsp;Le&nbsp;Floch
+									</span>
+								</h1>
+								<div className="mt-6 space-y-7 text-base text-slate-200">
+									<p>
+										{`
+											→ CTO de la start-up ForMenu, 
+										`}
+										<br />
+										{`
+											→ Co-fondateur de la plateforme de mise en relation entre particuliers et maquilleuses professionnelles My-Makeup,
+										`}
+										<br />
+										{`
+											→ Co-fondateur de la plateforme e-commerce Artriste.cc,
+										`}
+										<br />
+										{`
+											→ Co-fondateur du groupe d'entrepreneurs ForHives,
+										`}
+									</p>
+									<p>
+										{`
+										Je suis un étudiant en alternance qui habite à Nantes. Dès mon plus
+										jeune âge, j'ai été fasciné par les possibilités infinies des
+										ordinateurs et des nouvelles technologies.
+										`}
+									</p>
+									<p>
+										{`
+										Alors que je continue
+										d'apprendre et d'explorer cet univers en constant évolution, je suis
+										motivé par ma passion de découvrir de nouveaux mondes et leurs
+										nouvelles règles et mes rêves.
+										`}
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</section>
-
 			<section
-				className=" relative z-10 z-20 box-border flex
-      h-[100vh] w-full snap-start flex-col flex-wrap
-      items-start justify-center text-sky-50"
+				className="relative z-20 box-border flex
+      h-[100vh] w-full -translate-y-[30px] transform snap-start
+      flex-col flex-wrap items-start justify-center pb-[30px] text-sky-50"
 			>
 				<div className={'absolute -z-10 h-full w-full'}>
 					<Image
@@ -125,14 +174,19 @@ export default function Home() {
 						alt={''}
 						className={
 							'absolute left-0 top-0 ' +
-							'mix-difference -z-10 bg-slate-900 object-cover opacity-50 '
+							'mix-difference -z-10 bg-slate-1000 object-cover opacity-50 '
 						}
 						fill={true}
 						loading="eager"
 					/>
 					<div
 						className={
-							'mask-image absolute left-0 top-0 h-full w-full bg-gradient-to-r from-black via-[#000000] to-transparent'
+							'mask-image absolute left-0 top-0 h-full w-full bg-gradient-to-r from-[#000101] via-[#000101] to-transparent'
+						}
+					/>
+					<div
+						className={
+							'mask-image-bottom absolute left-0 top-0 h-full w-full bg-gradient-to-t from-[#000101] via-[#000101] to-transparent'
 						}
 					/>
 				</div>
@@ -144,7 +198,7 @@ export default function Home() {
 				<Link
 					type="button"
 					className="mx-12 my-12 inline-flex items-center rounded-full border border-transparent bg-glow-500 px-6 py-3 font-medium
-                    text-black text-black shadow-sm hover:bg-glow-600 focus:outline-none focus:ring-2 focus:ring-indigo-500
+                    text-black shadow-sm hover:bg-glow-600 focus:outline-none focus:ring-2 focus:ring-indigo-500
                     focus:ring-offset-2 lg:mx-80"
 					href={'/projet'}
 				>
