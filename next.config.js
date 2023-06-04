a2ad415a9f5d8b8ba80185b090e277c477fbf6b0
Cinquin Andy <andy.cinquin@infotel.com>
2023-06-04 15:15:58 +0200

    article & image with fallback (bcs the images are up 5% of the time)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
-
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**.lightin.io',
+			},
+			{
+				protocol: 'http',
+				hostname: '**.lightin.io',
+			},
+		],
+	},
 	i18n: {
 		locales: ['fr'],
 		defaultLocale: 'fr',
--- pages/projet.js
+++ pages/projet.js
@@ -4,12 +4,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react'
 import { Navbar } from '../components/navbar'
+import ImageWithFallback from '../components/ImageWithFallback'
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(' ')
 }
 
 export default function Project({ projects }) {
+	const [imgSrcFallback, setImgSrcFallback] = React.useState('')
 	return (
 		<div className="min-h-screen bg-slate-900">
 			<Head>
@@ -48,17 +50,29 @@ export default function Project({ projects }) {
 					{projects.map((project, projectIdx) => (
 						<Link
 							key={project.attributes.title}
-							className="flex flex-col-reverse overflow-hidden rounded-lg border-2 border-slate-900
-                            p-8 transition duration-150 ease-in-out
-                            hover:border-glow-600 hover:shadow-lg lg:grid lg:grid-cols-12  lg:gap-x-8"
+							className="relative flex flex-col-reverse overflow-hidden rounded-lg
+                            border-2 border-slate-1000/75 p-8 shadow-2xl transition duration-150
+                            ease-in-out hover:border-glow-600 hover:shadow-lg lg:grid lg:grid-cols-12  lg:gap-x-8"
 							href={'/projet/' + project.attributes.title}
 						>
+							<Image
+								src={'/abstract_shape.svg'}
+								alt={'abstract shape'}
+								className={
+									(projectIdx % 2 === 0
+										? '-left-20 rotate-12'
+										: '-right-20 -rotate-12') +
+									' absolute -bottom-20 z-10 rotate-12 transform opacity-10 invert'
+								}
+								width={300}
+								height={300}
+							/>
 							<div
 								className={classNames(
 									projectIdx % 2 === 0
 										? 'lg:col-start-1'
 										: 'lg:col-start-8 xl:col-start-9',
-									'mt-6 lg:col-span-5 lg:row-start-1 lg:mt-0 xl:col-span-4'
+									'z-20 mt-6 lg:col-span-5 lg:row-start-1 lg:mt-0 xl:col-span-4'
 								)}
 							>
 								<h3 className="text-lg font-medium text-gray-50">
@@ -73,7 +87,7 @@ export default function Project({ projects }) {
 									className="my-12 inline-flex items-center rounded-full border border-transparent bg-glow-500 px-3.5 py-2 text-sm font-medium leading-4
                                      text-black shadow-sm hover:bg-glow-600 focus:bg-glow-600 focus:outline-none focus:ring-2 focus:ring-offset-2"
 								>
-									en savoir plus
+									En savoir plus
 								</div>
 							</div>
 
@@ -85,14 +99,17 @@ export default function Project({ projects }) {
 									'flex-auto lg:col-span-7 lg:row-start-1 xl:col-span-8'
 								)}
 							>
-								<div className="aspect-w-5 aspect-h-2 overflow-hidden rounded-lg bg-gray-100">
-									<img
+								<div className="overflow-hidden rounded-lg">
+									<ImageWithFallback
 										src={
 											'https://breval-api.lightin.io' +
 											project.attributes.media.data[0]?.attributes.url
 										}
+										fallbackSrc="/projets.png"
 										alt={project.attributes.media.data[0]?.attributes.name}
-										className="object-cover object-center"
+										className="w-full object-cover object-center"
+										width={500}
+										height={500}
 									/>
 								</div>
 							</div>
