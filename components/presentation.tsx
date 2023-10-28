95f4699b648fa6849b4aeb7014e0d8de5777c61e
breval <breval2000@live.fr>
2023-10-28 14:35:40 +0200

    Refactor navbar and presentation components, update project page, and modify global styles

--- components/presentation.tsx
+++ components/presentation.tsx
@@ -19,7 +19,7 @@ export function Presentation() {
 							<div className="flex max-w-xs px-2.5 lg:max-w-none">
 								<div
 									className={
-										'w-auto rotate-2 rounded-2xl bg-glow-500/30 p-10 shadow-2xl'
+										'animated-image z-10 w-auto rotate-2 rounded-2xl bg-glow-500/0 p-10 shadow-2xl'
 									}
 								>
 									<Image
@@ -30,47 +30,53 @@ export function Presentation() {
 										className="aspect-square object-cover object-center"
 									/>
 								</div>
+								<div
+									className={
+										'animated-image-bg absolute h-[250px] w-[250px] rotate-2 rounded-2xl bg-glow-500/30 p-10 shadow-2xl'
+									}
+								></div>
 							</div>
 						</div>
 						<div className="flex h-full flex-col items-start justify-center lg:order-first lg:row-span-2">
 							<h3 className="text-4xl font-bold tracking-tight text-slate-200 lg:text-5xl">
-								{`Bonjour, je m'appelle `}
+								{`Hello, my name is `}
 								<span className="text-green-600">
 									Bréval&nbsp;Le&nbsp;Floch
 								</span>
 							</h3>
 							<div className="mt-6 space-y-7 text-base text-slate-300">
 								<p>
+									→ CTO of the start-up{' '}
+									<a className="text-glow-500" href="https://formenu.fr">
+										ForMenu
+									</a>
+									<br />→ Co-founder of{' '}
+									<a className="text-glow-500" href="https://my-makeup.fr">
+										My-Makeup
+									</a>
+									, a platform to reference makeup artists
+									<br />→ Co-founder of{' '}
+									<a className="text-glow-500" href="https://artriste.cc">
+										Artriste
+									</a>
+									, an e-commerce website for art
+									<br />→ Co-founder of{' '}
+									<a className="text-glow-500" href="https://forhives.fr">
+										ForHives
+									</a>
+									, the hub to our bees
+								</p>
+								<p>
 									{`
-											→ CTO de la start-up ForMenu, 
-										`}
-									<br />
-									{`
-											→ Co-fondateur de la plateforme de mise en relation entre particuliers et maquilleuses professionnelles My-Makeup,
-										`}
-									<br />
-									{`
-											→ Co-fondateur de la plateforme e-commerce Artriste.cc,
-										`}
-									<br />
-									{`
-											→ Co-fondateur du groupe d'entrepreneurs ForHives,
+										I'm a work-study student living in Nantes. From my youngest age, I am very
+										fascinated by the infinite possibilities of computers and new technologies.
 										`}
 								</p>
 								<p>
 									{`
-										Je suis un étudiant en alternance qui habite à Nantes. Dès mon plus
-										jeune âge, j'ai été fasciné par les possibilités infinies des
-										ordinateurs et des nouvelles technologies.
-										`}
-								</p>
-								<p>
-									{`
-										Alors que je continue
-										d'apprendre et d'explorer cet univers en constant évolution, je suis
-										motivé par ma passion de découvrir de nouveaux mondes et leurs
-										nouvelles règles et mes rêves.
-										`}
+                                    As I continue to learn and explore this ever-evolving universe,
+                                    I'm driven by my passion to discover new worlds and their new rules.
+                                    `}
 								</p>
 							</div>
 						</div>
