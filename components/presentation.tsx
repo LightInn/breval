c11e4fef9dce04cfaa27103de9a8d5c9ca11b32d
breval <breval2000@live.fr>
2023-10-28 20:50:35 +0200

    Refactor code for Hero component and Presentation component
    - Refactored import statements to follow consistent formatting
    - Reorganized HTML structure in the Hero component for better readability
    - Updated class names and attributes in the Hero component to match design requirements
    - Adjusted styling of elements in the Presentation component
    
    Note: Removed unnecessary empty lines from globals.css file.

--- components/presentation.tsx
+++ components/presentation.tsx
@@ -40,9 +40,7 @@ export function Presentation() {
 						<div className="flex h-full flex-col items-start justify-center lg:order-first lg:row-span-2">
 							<h3 className="text-4xl font-bold tracking-tight text-slate-200 lg:text-5xl">
 								{`Hello, my name is `}
-								<span className="text-glow-600">
-									Bréval&nbsp;Le&nbsp;Floch
-								</span>
+								<span className="text-glow-600">Bréval&nbsp;Le&nbsp;Floch</span>
 							</h3>
 							<div className="mt-6 space-y-7 text-base text-slate-300">
 								<p>
