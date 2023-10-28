5b1e9b5076729124a5b9956ffc2c33333991966b
Breval Le Floch <breval2000@live.fr>
2023-10-28 23:46:20 +0200

    Refactor project.js: Adjust layout and styling for project details (#138)
    - Added a gap between elements in the flex container
    - Modified the width of a button element to occupy half of its parent container's width

--- pages/projet.js
+++ pages/projet.js
@@ -108,7 +108,7 @@ export default function Project({ projects }) {
 										</div>
 									</div>
 
-									<div className={'flex flex-col justify-start'}>
+									<div className={'flex flex-col justify-start gap-4 py-4'}>
 										<h3 className="text-lg font-medium text-gray-50">
 											{' '}
 											{project.attributes.title}
@@ -118,7 +118,7 @@ export default function Project({ projects }) {
 											{project.attributes.short_description}
 										</p>
 										<div
-											className="inline-flex items-center rounded-full border border-transparent bg-glow-500 p-12 px-3.5 py-2 text-sm font-medium leading-4
+											className=" w-1/2 rounded-full border border-transparent bg-glow-500 px-8 py-2 text-sm font-medium leading-4
                                      text-black shadow-sm hover:bg-glow-600 focus:bg-glow-600 focus:outline-none focus:ring-2 focus:ring-offset-2"
 										>
 											En savoir plus
