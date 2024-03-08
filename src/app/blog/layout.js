7410b1df482ee63eea53f4fb0a7a5f8a20245035
Breval <breval2000@live.fr>
2024-03-08 09:53:05 +0100

    next font error

--- src/app/blog/layout.js
+++ src/app/blog/layout.js
@@ -1,5 +1,4 @@
-import { cx } from "/src/utils";
-import { Inter, Manrope } from "next/font/google";
+import {cx} from "/src/utils";
 import Header from "/src/components/Header";
 import siteMetadata from "../../utils/siteMetaData";
 import Script from "next/script";
@@ -7,59 +6,47 @@ import Footer from "/src/components/Footer";
 
 export const revalidate = 60;
 
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-in",
-});
-
-const manrope = Manrope({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-mr",
-});
-
 export const metadata = {
-  metadataBase: new URL(siteMetadata.siteUrl),
-  title: {
-    template: `%s | ${siteMetadata.title}`,
-    default: siteMetadata.title, // a default is required when creating a template
-  },
-  description: siteMetadata.description,
-  openGraph: {
-    title: siteMetadata.title,
-    description: siteMetadata.description,
-    url: siteMetadata.siteUrl,
-    siteName: siteMetadata.title,
-    images: [siteMetadata.socialBanner],
-    locale: "fr_FR",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
+    metadataBase: new URL(siteMetadata.siteUrl),
+    title: {
+        template: `%s | ${siteMetadata.title}`,
+        default: siteMetadata.title, // a default is required when creating a template
     },
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteMetadata.title,
-    images: [siteMetadata.socialBanner],
-  },
+    description: siteMetadata.description,
+    openGraph: {
+        title: siteMetadata.title,
+        description: siteMetadata.description,
+        url: siteMetadata.siteUrl,
+        siteName: siteMetadata.title,
+        images: [siteMetadata.socialBanner],
+        locale: "fr_FR",
+        type: "website",
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            noimageindex: true,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: siteMetadata.title,
+        images: [siteMetadata.socialBanner],
+    },
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <div className={cx(inter.variable, manrope.variable, "font-mr bg-light")}>
-      <Header />
-      {children}
-      <Footer />
-    </div>
-  );
+export default function RootLayout({children}) {
+    return (
+        <div className={cx("font-mr bg-light")}>
+            <Header/>
+            {children}
+            <Footer/>
+        </div>
+    );
 }
