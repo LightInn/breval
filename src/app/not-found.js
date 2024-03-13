00865387b29ea45a2704ed0619056be7baa983a6
breval <breval2000@live.fr>
2024-03-13 02:11:35 +0100

    load old stuff

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,30 +1,23 @@
 import Link from "next/link";
 
 export default function NotFound() {
-  return (
-    <main className="my-32 w-full bg-accent  flex justify-center font-mr">
-      <div className="relative flex flex-col items-center justify-center">
-        <h1
-          className={`inline-block text-dark
-      text-6xl font-bold w-full capitalize xl:text-8xl text-center`}
-        >
-          404
-        </h1>
-        <h2
-          className={`inline-block text-dark
-      text-5xl font-bold w-full capitalize xl:text-6xl text-center mt-4 tracking-wide leading-snug`}
-        >
-          Page Not Found!
-        </h2>
-        <Link
-          href="/"
-          className="self-center mt-8 inline-block rounded-lg border-2 border-solid  px-4 py-2
-        font-semibold text-light hover:border-dark hover:bg-light hover:text-dark
-        "
-        >
-          Go To Home
-        </Link>
-      </div>
-    </main>
-  );
+    return (<main className="h-screen w-full flex flex-col justify-center items-center bg-white">
+            <h1 className="text-9xl font-extrabold text-accent tracking-widest">404</h1>
+            <div className="bg-glow-500 px-2 text-sm rounded rotate-12 absolute">
+                Page Not Found
+            </div>
+            <button className="mt-5">
+                <Link href={"/"}
+                    className="relative inline-block text-sm font-medium text-black group active:text-orange-500 focus:outline-none focus:ring"
+                >
+        <span
+            className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-accent group-hover:translate-y-0 group-hover:translate-x-0"
+        ></span>
+
+                    <span className="relative block px-8 py-3 bg-accent border border-current">
+          <router-link to="/">Go Home</router-link>
+        </span>
+                </Link>
+            </button>
+        </main>);
 }
