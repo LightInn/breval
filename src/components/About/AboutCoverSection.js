2509f453b4e4f64d08d219b9adb72316e8d8f831
breval <breval2000@live.fr>
2024-03-22 23:42:39 +0100

    BUG

--- src/components/About/AboutCoverSection.js
+++ src/components/About/AboutCoverSection.js
@@ -4,6 +4,7 @@ import profileCharacter from "../../../public/avatar.svg";
 import Avatar from "/src/components/About/Avatar";
 import {SocialIcons} from "@/components/Home/Social";
 import {GithubIcon,DribbbleIcon,MoonIcon,SunIcon,LinkedinIcon,TwitterIcon} from "@/components/Icons";
+// import AvatarSVG from "/public/avatar.svg";
 
 const AboutCoverSection = ({background = null}) => {
     return (
@@ -12,15 +13,16 @@ const AboutCoverSection = ({background = null}) => {
             <div className="p-20 w-full h-full flex justify-center items-center backdrop-blur-lg">
 
 
-                <div className="flex flex-col items-end w-1/4">
+                <div className="flex flex-col justify-center items-center md:items-end w-full md:w-1/4 ">
 
+                    {/*<Image src={AvatarSVG} alt="avatar of Bréval" width={250} height={250} className="md:hidden my-10" />*/}
 
-                    <h2 className="font-bold capitalize text-4xl md:text-6xl  text-center lg:text-right  md:bg-white/30 md:rounded-2xl md:p-6 message-bulle">
+                    <h2 className="font-bold capitalize text-white text-4xl md:text-6xl  text-center lg:text-right  md:bg-white/30 md:rounded-2xl md:p-6 md:message-bulle ">
                         Hi, I&apos;m <br/>
                         <bold>Bréval</bold>
                         .
                     </h2>
-                    <p className="font-medium capitalize md:text-2xl text-right  md:bg-white/30 md:rounded-2xl md:p-20 md:mt-8 message-bulle">
+                    <p className="font-medium capitalize md:text-2xl md:text-right  md:bg-white/30 md:rounded-2xl md:p-20 md:mt-8 md:message-bulle text-white">
                         I&apos;m a creative developer <br/>
                         from France, Loire Atlantique.
                     </p>
