d88d0b11950bfc0a00e193d61a596350169f27a5
breval <breval2000@live.fr>
2024-03-26 01:02:25 +0100

    Update Navbar and Playground components with mobile view functionality and theme options.
    - Added state for mobile menu toggle in Navbar.
    - Implemented mobile view layout and menu toggle functionality in Navbar.
    - Enhanced theme display and image handling in Playground component.
    - Improved styling and reload feature in Playground.

--- src/components/About/Navbar.js
+++ src/components/About/Navbar.js
@@ -1,9 +1,15 @@
+"use client";
 import css from "/src/styles/Navbar.module.css";
 import Link from "next/link";
-import React from "react";
+import React, {useState} from "react";
 import NavbarOfficial from "@/components/navbar";
+import Image from "next/image";
 
 function Navbar() {
+
+  const [isMobileOpen, setIsMobileOpen] = useState(false);
+
+
   return (
     <>
       <div className={css.navBackground + " hidden md:flex"}>
@@ -68,8 +74,117 @@ function Navbar() {
           </Link>
         </ul>
       </div>
-      <div className="block md:hidden">
-        <NavbarOfficial />
+      <div className="block md:hidden fixed w-screen z-50">
+        {/* -------------------------------------------------------------------------------------------------------------------    Mobile view under md*/}
+        <div
+            className={
+                "flex flex-col md:hidden  w-full " +
+                (isMobileOpen ? "bg-dynamic-vibrant-light" : "")
+            }
+        >
+          <div
+              className={
+                "flex h-[60px] bg-dynamic-vibrant justify-between items-center px-4"
+              }
+          >
+            <Link
+                href="/public"
+                className={`m-0 flex origin-center 
+                     items-center justify-center rounded-full p-0 transition-all
+                     duration-100  ease-in-out h-full w-16 no-underline`}
+            >
+              <Image
+                  src="/logo.png"
+                  width={100}
+                  height={100}
+                  alt="Logo signature de Bréval Le Floch"
+              />
+            </Link>
+
+            <button
+                type="button"
+                onClick={() => setIsMobileOpen(!isMobileOpen)}
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+            >
+              <span className="absolute -inset-0.5"></span>
+
+              <span className="sr-only">Open main menu</span>
+
+              <svg
+                  x-description="Icon when menu is closed."
+                  className={"block h-6 w-6" + (isMobileOpen ? " hidden" : "")}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+              >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                ></path>
+              </svg>
+
+              <svg
+                  x-description="Icon when menu is open."
+                  className={"h-6 w-6" + (isMobileOpen ? " block" : " hidden")}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+              >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+          </div>
+
+          <div className={" h-screen " + (isMobileOpen ? "block" : "hidden")}>
+            <div
+                className={"px-2 pt-2 pb-3 space-y-1"}
+                id="mobile-menu"
+                aria-expanded="false"
+            >
+              <Link
+                  href="/"
+                  className={
+                    "bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+                  }
+              >
+                Home
+              </Link>
+              <Link
+                  href="/projects"
+                  className={
+                    "text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  }
+              >
+                Projects
+              </Link>
+              <Link
+                  href="/blog"
+                  className={
+                    "text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  }
+              >
+                Blog
+              </Link>
+              <Link
+                  href="/about"
+                  className={"text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"}
+              >
+                About
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
