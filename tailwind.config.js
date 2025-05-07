7773fd4ecb4fa4b0e313eecd794f17ff5c6cda2a
Breval <breval2000@live.fr>
2025-05-07 14:16:56 +0200

    feat: white theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,17 @@ module.exports = {
 					1000: '#000101',
 				},
 				glow: {
-					500: '#ffc6d3',
-					600: '#ff99b1',
+					100: '#ffc6cb',
+					200: '#ffc6d3',
+					300: '#ff9ba8',
+					400: '#ea8fa7',
+					500: '#ff99b1',
+					600: '#cc7a8d',
+					700: '#995b69',
+					800: '#653c46',
+					900: '#311d22',
 				},
-				light: '#f4faf7',
+				light: '#e7ecea',
 				accent: '#ff99b1',
 				gray: '#706262',
 				'dynamic-vibrant': 'var(--vibrant)',
@@ -56,18 +63,30 @@ module.exports = {
 				'sakura-tree': 'url(/sakura_tree_outline.webp)',
 				'dynamic-bg': 'var(--bg-img)',
 			},
-
 			fontFamily: {
 				mr: ['var(--font-mr)'],
 				in: ['var(--font-in)'],
 			},
 			animation: {
+				'scroll-down': 'scroll-down 2.5s ease-in-out infinite',
+				'fade-in-up': 'fade-in-up 1s ease-out forwards',
 				roll: 'roll 24s linear infinite',
+				bounce: 'bounce 2s infinite',
+				pulse: 'pulse 2s infinite',
 			},
 			keyframes: {
 				roll: {
 					'0%': { transform: 'translateX(100%)' },
 					'100%': { transform: 'translateX(-100%)' },
+				},
+				'scroll-down': {
+					'0%': { opacity: '0', transform: 'translateY(-5px)' },
+					'50%': { opacity: '1', transform: 'translateY(5px)' },
+					'100%': { opacity: '0', transform: 'translateY(15px)' },
+				},
+				'fade-in-up': {
+					'0%': { opacity: '0', transform: 'translateY(10px)' },
+					'100%': { opacity: '1', transform: 'translateY(0)' },
 				},
 			},
 			screens: {
