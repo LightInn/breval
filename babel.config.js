8f2d41e09cc111da5015291cecd68231b56f8cd0
Breval <breval2000@live.fr>
2025-05-23 16:06:36 +0200

    Update sitemap-0.xml with new lastmod dates and maintain existing URLs

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-react', { runtime: 'automatic' }],
-    'next/babel' // Add this if you are using Next.js specific Babel features like `next/image`
-  ],
-};
